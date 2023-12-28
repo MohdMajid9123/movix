7b530661583f963100e86d3948bbcb31d1a239f9
Md <mohdmajid9123@gmail.com>
2023-12-28 14:32:00 +0530

    change some logic they are not woking

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -8,12 +8,13 @@ import { useNavigate } from "react-router-dom";
 import "./HeroBanner.scss";
 import useFetch from "../../../hooks/useFetch";
 import { useSelector } from "react-redux";
-// import Img from "../../../component/lazyLoadiImage/img";
 import Img from "../../../component/lazyLoadiImage/Img";
 import ContentWrapper from "../../../component/contentWrapper/ContentWrapper";
 
 const HeroBanner = () => {
   const [background, setBackground] = useState("");
+  console.log(background);
+
   const [query, setQuery] = useState("");
 
   const navigate = useNavigate();
@@ -22,15 +23,20 @@ const HeroBanner = () => {
   const { data, loading } = useFetch("/movie/upcoming");
 
   useEffect(() => {
-    const bg =
-      url?.backdrop +
-      data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
-    setBackground(bg);
-    console.log(background);
-  }, [data]);
+    const generateBackground = () => {
+      const randomMovie = data?.results?.[Math?.floor(Math?.random() * 20)];
+
+      if (url?.backdrop && randomMovie?.backdrop_path) {
+        const bg = url.backdrop + randomMovie.backdrop_path;
+        setBackground(bg);
+      }
+    };
+
+    generateBackground();
+  }, [data, url]);
 
   const searchQueryHandler = (e) => {
-    if (e.key === "Enter" && query.length > 0) {
+    if ((e.key === "Enter" || e.type === "click") && query.length > 0) {
       navigate(`/search/${query}`);
     }
   };
@@ -56,7 +62,7 @@ const HeroBanner = () => {
               onKeyUp={searchQueryHandler}
               onChange={(e) => setQuery(e.target.value)}
             />
-            <button>Search</button>
+            <button onClick={searchQueryHandler}>Search</button>
           </div>
         </div>
       </ContentWrapper>
